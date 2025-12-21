fbae0df609a917c5114a87c4735d0e6321c768e3
ali_maameri02 <ali.maameri@univ-constanine2.com>
2025-12-21 16:41:02 +0100

    fix logo

--- hvmc-store/src/components/Landing/Footer.tsx
+++ hvmc-store/src/components/Landing/Footer.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { Instagram, Facebook, Phone, Mail, MapPin } from 'lucide-react';
 import '@/lib/rtl.css'
 import { FaTiktok } from 'react-icons/fa';
-import logo from '@/assets/profile-lreMy55m.png';
+import logo from '@/assets/profile.png';
 
 export const Footer = () => {
   const { t, i18n } = useTranslation();
